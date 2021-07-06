8519eea14fd6e2c13d331a5130384cb87b0e4ff0
Atom-cell <81373684+Atom-cell@users.noreply.github.com>
2021-07-06 23:53:34 +0500

    Update template.blade.php

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -5,8 +5,8 @@
             Khareed Lo- @yield('title')
         </title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" charset="UTF-8">
-        <link rel="stylesheet" href="{{asset('/css/main.css')}}">
-        <link rel="stylesheet" href="{{asset('/css/cart.css')}}">
+        <link rel="stylesheet" href="/css/main.css">
+        <link rel="stylesheet" href="/css/cart.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
